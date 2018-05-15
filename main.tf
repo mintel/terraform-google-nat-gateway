588b6d6502d5f627a1d0d483434b26e747083b72
Francesco Ciocchetti <fciocchetti@mintel.com>
2018-05-15 09:30:58 +0100

    Enforce version of upstream module so that random changes in master don't recreate my infrastructure

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ data "google_compute_address" "default" {
 }
 
 module "nat-gateway" {
-  source             = "github.com/GoogleCloudPlatform/terraform-google-managed-instance-group"
+  source             = "github.com/GoogleCloudPlatform/terraform-google-managed-instance-group?ref=1.1.7"
   project            = "${var.project}"
   region             = "${var.region}"
   zone               = "${var.zone == "" ? lookup(var.region_params["${var.region}"], "zone") : var.zone}"
